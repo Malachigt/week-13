ae350a69f9bc3f102f1f9d7b9fbf94f49600d51e
malachi <malachigt@gmail.com>
2024-02-29 10:56:59 -0500

    tip couculater express application created

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ res.send("hello, stranger");
 app.get("/greeting/:name",(req, res) => {
 res.send(`Hello, `+ req.params.name);
 })
+app.get("/tip/:total/:tipPercentage",(req,res) => {
+    const total = parseFloat(req.params.total)
+    const percent = parseFloat(req.params.tipPercentage)
+    const result = total*percent/100
+    res.send(result.toString());;
+});
+
 
 app.listen(3000);
 
